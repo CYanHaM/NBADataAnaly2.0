b77698b26e3cca0d8451194a9e337fcfd0fa5542
admin <admin@idea-PC>
2015-04-22 22:36:05 +0800

    matchpo refreshing 2nd

--- src/PO/MatchPO.java
+++ src/PO/MatchPO.java
@@ -20,6 +20,9 @@ public class MatchPO implements Serializable{
 	public String score4;                                 //第四节比分
 	public String scoreExtra;                             //加时赛比分
 	public ArrayList<PlayerTechMPO> playerStatistic;      //球员技术统计
+	public String scoringChampion;                            //得分王
+	public String reboundChampion;                          //篮板王
+	public String assistChampion;                      //助攻王
 	public int ifHomeTeamWin;                         //主队是否胜利
 	public int ifGuestTeamWin;                        //客队是否胜利
 	public int homeTeamDeffensiveRebound;              //主队防守篮板
