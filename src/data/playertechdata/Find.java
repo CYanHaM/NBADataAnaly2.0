96f7c77441bbf789cdddc9cdf17a278f3b777c95
Mao-xy <15850563830@139.com>
2015-04-24 11:01:20 +0800

    完成playerTech的升序降序排列和查找当天热点球员

--- src/data/playertechdata/Find.java
+++ src/data/playertechdata/Find.java
@@ -1,6 +1,8 @@
 package data.playertechdata;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import PO.PlayerTechMPO;
 import PO.PlayerTechPO;
@@ -13,10 +15,67 @@ public class Find implements FindDataService {
 	OperateWithFile owf = new OperateWithFile();
 	
 	@Override
-	public ArrayList<PlayerTechMPO> findHotPlayerToday(String date, String keyword)  {
+	public ArrayList<PlayerTechMPO> findHotPlayerToday(String date, final String keyword)  {
 		// TODO Auto-generated method stub
+		//筛选今天的比赛数据
+		ArrayList<PlayerTechMPO> list = owf.readMPO();
+		ArrayList<PlayerTechMPO> res = new ArrayList<PlayerTechMPO>();
+		int size = list.size();
+		for(int i=0;i<size;i++){
+			if(list.get(i).date.equals(date)){
+				res.add(list.get(i));
+			}
+		}
 		
-    	return null;
+		//进行排序
+		Comparator<PlayerTechMPO> comparator = new Comparator<PlayerTechMPO>(){  
+			public int compare(PlayerTechMPO p2, PlayerTechMPO p1) {   
+				//重写比较方法
+				switch(keyword){
+				case "time":
+					return p1.time-p2.time;
+				case "shotin":
+					return p1.shotIn-p2.shotIn;
+				case "shot":
+					return p1.shot-p2.shot;
+				case "threeshotin":
+					return p1.threeShotIn-p2.threeShotIn;
+				case "threeshot":
+					return p1.threeShot-p2.threeShot;
+				case "penaltyshotin":
+					return p1.penaltyShotIn-p2.penaltyShotIn;
+				case "penaltyshot":
+					return p1.penaltyShot-p2.penaltyShot;
+				case "offensiverebound":
+					return p1.offensiveRebound-p2.offensiveRebound;
+				case "defensiverebound":
+					return p1.defensiveRebound-p2.defensiveRebound;
+				case "rebound":
+					return p1.rebound-p2.rebound;
+				case "secondaryAttack":
+					return p1.secondaryAttack-p2.secondaryAttack;	
+				case "steal":
+					return p1.steal-p2.steal;
+				case "blockshot":
+					return p1.blockShot-p2.blockShot;
+				case "fault":
+					return p1.fault-p2.fault;
+				case "foul":
+					return p1.foul-p2.foul;
+				case "score":
+					return p1.score-p2.score;
+				default:
+					System.out.println("wrong type");
+					return 0;
+				}
+			}  
+		}; 
+		Collections.sort(res, comparator);
+		ArrayList<PlayerTechMPO> result = new ArrayList<PlayerTechMPO>();
+		for(int i=0;i<5;i++){
+			result.add(res.get(i));
+		}
+		return result;
 	}
 
 	@Override
--- src/data/playertechdata/Show.java
+++ src/data/playertechdata/Show.java
@@ -1,6 +1,8 @@
 package data.playertechdata;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import PO.PlayerTechPO;
 import dataservice.playertechdataservice.ShowDataService;
@@ -29,15 +31,171 @@ public class Show implements ShowDataService {
 	}
 
 	@Override
-	public ArrayList<PlayerTechPO> ascend(String type)  {
+	public ArrayList<PlayerTechPO> ascend(final String type)  {
 		// TODO Auto-generated method stub
-		return null;
+		Comparator<PlayerTechPO> comparator = new Comparator<PlayerTechPO>(){  
+			public int compare(PlayerTechPO p1, PlayerTechPO p2) {   
+				//重写比较方法
+				switch(type){
+				case "gamenum":
+					return p1.gameNum-p2.gameNum;
+				case "startingnum":
+					return p1.startingNum-p2.startingNum;
+				case "rebound":
+					return p1.rebound-p2.rebound;
+				case "secondaryAttack":
+					return p1.secondaryAttack-p2.secondaryAttack;
+				case "time":
+					return p1.time-p2.time;
+				case "offensivenum":
+					return p1.offensiveNum-p2.offensiveNum;
+				case "defensivenum":
+					return p1.defensiveNum-p2.defensiveNum;
+				case "steal":
+					return p1.steal-p2.steal;
+				case "blockshot":
+					return p1.blockShot-p2.blockShot;
+				case "fault":
+					return p1.fault-p2.fault;
+				case "foul":
+					return p1.foul-p2.foul;
+				case "score":
+					return p1.score-p2.score;
+				case "shotin":
+					return p1.shotIn-p2.shotIn;
+				case "shot":
+					return p1.shot-p2.shot;
+				case "threeshotin":
+					return p1.threeShotIn-p2.threeShotIn;
+				case "threeshot":
+					return p1.threeShot-p2.threeShot;
+				case "penaltyshotin":
+					return p1.penaltyShotIn-p2.penaltyShotIn;
+				case "penaltyshot":
+					return p1.penaltyShot-p2.penaltyShot;
+				case "shotinrate":
+					return (int) (p1.shotInRate-p2.shotInRate);
+				case "threeshotinrate":
+					return (int) (p1.threeShotInRate-p2.threeShotInRate);
+				case "penaltyshotinrate":
+					return (int) (p1.penaltyShotInRate-p2.penaltyShotInRate);
+				case "efficiency":
+					return (int) (p1.efficiency-p2.efficiency);
+				case "gmscefficiency":
+					return (int) (p1.GmScEfficiency-p2.GmScEfficiency);
+				case "trueshotinrate":
+					return (int) (p1.trueShotInRate-p2.trueShotInRate);
+				case "shootingefficiency":
+					return (int) (p1.shootingEfficiency-p2.shootingEfficiency);
+				case "reboundrate":
+					return (int) (p1.reboundRate-p2.reboundRate);
+				case "offensivereboundrate":
+					return (int) (p1.offensiveReboundRate-p2.offensiveReboundRate);
+				case "defensivereboundrate":
+					return (int) (p1.defensiveReboundRate-p2.defensiveReboundRate);
+				case "secondaryattackrate":
+					return (int) (p1.secondaryAttackRate-p2.secondaryAttackRate);
+				case "stealrate":
+					return (int) (p1.stealRate-p2.stealRate);
+				case "blockshotrate":
+					return (int) (p1.blockShotRate-p2.blockShotRate);
+				case "faultrate":
+					return (int) (p1.faultRate-p2.faultRate);
+				case "usagerate":
+					return (int) (p1.usageRate-p2.usageRate);
+				default:
+					System.out.println("wrong type");
+					return 0;
+				}
+			}  
+		}; 
+		ArrayList<PlayerTechPO> list = owf.read();
+		Collections.sort(list, comparator);
+		return list;
 	}
 
 	@Override
-	public ArrayList<PlayerTechPO> descend(String type){
+	public ArrayList<PlayerTechPO> descend(final String type){
 		// TODO Auto-generated method stub
-		return null;
+		Comparator<PlayerTechPO> comparator = new Comparator<PlayerTechPO>(){  
+			public int compare(PlayerTechPO p2, PlayerTechPO p1) {   
+				//重写比较方法
+				switch(type){
+				case "gamenum":
+					return p1.gameNum-p2.gameNum;
+				case "startingnum":
+					return p1.startingNum-p2.startingNum;
+				case "rebound":
+					return p1.rebound-p2.rebound;
+				case "secondaryAttack":
+					return p1.secondaryAttack-p2.secondaryAttack;
+				case "time":
+					return p1.time-p2.time;
+				case "offensivenum":
+					return p1.offensiveNum-p2.offensiveNum;
+				case "defensivenum":
+					return p1.defensiveNum-p2.defensiveNum;
+				case "steal":
+					return p1.steal-p2.steal;
+				case "blockshot":
+					return p1.blockShot-p2.blockShot;
+				case "fault":
+					return p1.fault-p2.fault;
+				case "foul":
+					return p1.foul-p2.foul;
+				case "score":
+					return p1.score-p2.score;
+				case "shotin":
+					return p1.shotIn-p2.shotIn;
+				case "shot":
+					return p1.shot-p2.shot;
+				case "threeshotin":
+					return p1.threeShotIn-p2.threeShotIn;
+				case "threeshot":
+					return p1.threeShot-p2.threeShot;
+				case "penaltyshotin":
+					return p1.penaltyShotIn-p2.penaltyShotIn;
+				case "penaltyshot":
+					return p1.penaltyShot-p2.penaltyShot;
+				case "shotinrate":
+					return (int) (p1.shotInRate-p2.shotInRate);
+				case "threeshotinrate":
+					return (int) (p1.threeShotInRate-p2.threeShotInRate);
+				case "penaltyshotinrate":
+					return (int) (p1.penaltyShotInRate-p2.penaltyShotInRate);
+				case "efficiency":
+					return (int) (p1.efficiency-p2.efficiency);
+				case "gmscefficiency":
+					return (int) (p1.GmScEfficiency-p2.GmScEfficiency);
+				case "trueshotinrate":
+					return (int) (p1.trueShotInRate-p2.trueShotInRate);
+				case "shootingefficiency":
+					return (int) (p1.shootingEfficiency-p2.shootingEfficiency);
+				case "reboundrate":
+					return (int) (p1.reboundRate-p2.reboundRate);
+				case "offensivereboundrate":
+					return (int) (p1.offensiveReboundRate-p2.offensiveReboundRate);
+				case "defensivereboundrate":
+					return (int) (p1.defensiveReboundRate-p2.defensiveReboundRate);
+				case "secondaryattackrate":
+					return (int) (p1.secondaryAttackRate-p2.secondaryAttackRate);
+				case "stealrate":
+					return (int) (p1.stealRate-p2.stealRate);
+				case "blockshotrate":
+					return (int) (p1.blockShotRate-p2.blockShotRate);
+				case "faultrate":
+					return (int) (p1.faultRate-p2.faultRate);
+				case "usagerate":
+					return (int) (p1.usageRate-p2.usageRate);
+				default:
+					System.out.println("wrong type");
+					return 0;
+				}
+			}  
+		}; 
+		ArrayList<PlayerTechPO> list = owf.read();
+		Collections.sort(list, comparator);
+		return list;
 	}
 
 }
