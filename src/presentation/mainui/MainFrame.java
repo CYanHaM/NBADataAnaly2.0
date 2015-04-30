c864dd21a47e9bbc4c88bdb3f584d1338438beac
blisscry <409230092@qq.com>
2015-04-30 14:25:37 +0800

    new data

--- src/presentation/mainui/MainFrame.java
+++ src/presentation/mainui/MainFrame.java
@@ -9,7 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
-import presentation.teamui.TeamInfoPanel;
+import presentation.matchui.MatchPanel;
 import blservice.playerinfoblservice.PlayerInfoService;
 import blservice.playertechblservice.ShowPlayerTechService;
 import blservice.teamblservice.TeamBLservice;
@@ -120,8 +120,8 @@ public class MainFrame extends JFrame{
 		
 		
 		MainFrame mf=new MainFrame();
-//		MatchPanel mp=new MatchPanel(mf);
-		TeamInfoPanel mp=new TeamInfoPanel(mf);
+		MatchPanel mp=new MatchPanel(mf);
+//		TeamInfoPanel mp=new TeamInfoPanel(mf);
 //		HotPlayerToday mp=new HotPlayerToday(mf);
 		mf.add(mp);
 		mf.repaint();
