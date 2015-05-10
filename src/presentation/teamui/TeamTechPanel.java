514019ecbea9ad087c6f57b20e513c991f4f5068
blisscry <409230092@qq.com>
2015-05-10 16:02:25 +0800

    refresh

--- src/presentation/teamui/TeamTechPanel.java
+++ src/presentation/teamui/TeamTechPanel.java
@@ -23,10 +23,12 @@ import javax.swing.table.TableColumnModel;
 
 import presentation.hotspotui.HotPlayerToday;
 import presentation.matchui.MatchPanel;
+import presentation.playerui.PlayerInfoPanel;
 import presentation.playerui.PlayerTechPanel;
 import presentation.preset.TeamTechPre;
 import TypeEnum.TeamTechEnum;
 import VO.TeamTechVO;
+import VO.TeamVO;
 
 
 public class TeamTechPanel extends JPanel implements ActionListener{
@@ -458,6 +460,79 @@ public class TeamTechPanel extends JPanel implements ActionListener{
 		}
 	}
 	
+	private String switchTeam(String name){
+		switch(name){
+		case "老鹰 Atlanta-Hawks":
+			return "ATL";
+		case "黄蜂 Charlotte-Hornets":
+			return "CHA";
+		case "热火 Miami-Heat":
+			return "MIA";
+		case "魔术 Orlando-Magic":
+			return "ORL";
+		case "奇才 Washington-Wizards":
+			return "WAS";
+			
+		case "公牛 Chicago-Bulls":
+			return "CHI";
+		case "骑士 Cleveland-Cavaliers":
+			return "CLE";
+		case "活塞 Detroit-Pistons":
+			return "DET";
+		case "步行者 Indiana-Pacers":
+			return "IND";
+		case "雄鹿 Milwaukee-Bucks":
+			return "MIL";
+			
+		case "凯尔特人 Boston-Celtics":
+			return "BOS";
+		case "篮网 Brooklyn-Nets":
+			return "BKN";
+		case "尼克斯 New York-Knicks":
+			return "NYK";
+		case "76人 Philadelphia-76ers":
+			return "PHI";
+		case "猛龙 Toronto-Raptors":
+			return "TOR";
+			
+			
+		case "勇士 Golden State-Warriors":
+			return "GSW";
+		case "快船 Los Angeles-Clippers":
+			return "LAC";
+		case "湖人 Los Angeles-Lakers":
+			return "LAL";
+		case "太阳 Phoenix-Suns":
+			return "PHX";
+		case "国王 Sacramento-Kings":
+			return "SAC";
+			
+		case "掘金 Denver-Nuggets":
+			return "DEN";
+		case "森林狼 Minnesota-Timberwolves":
+			return "MIN";
+		case "雷霆 Oklahoma City-Thunder":
+			return "OKC";
+		case "开拓者 Portland-Trail Blazers":
+			return "POR";
+		case "勇士 Utah-Jazz":
+			return "UTA";
+			
+		case "小牛 Dallas-Mavericks":
+			return "DAL";
+		case "火箭 Houston-Rockets":
+			return "HOU";
+		case "灰熊 Memphis-Grizzlies":
+			return "MEM";
+		case "鹈鹕 New Orleans-Pelicans":
+			return "NOP";
+		case "马刺 San Antonio-Spurs":
+			return "SAS";
+		default :
+				return null;
+		}
+	}
+	
 	//表格配置
 	public void table1_config(){
 		//------------------------------表格基本属性--------------------------
@@ -524,6 +599,23 @@ public class TeamTechPanel extends JPanel implements ActionListener{
 				}
 				
 			}
+		});
+		
+		teamtable1.addMouseListener(new MouseAdapter() {
+			 public void mouseClicked(MouseEvent e) {
+				   int row= teamtable1.getSelectedRow();
+	               int column= teamtable1.getSelectedColumn();
+	               if(column==1){
+	               //得到选中的单元格的值，表格中都是字符串
+	               Object value= teamtable1.getValueAt(row, column);
+	               TeamVO tvo=new TeamVO();
+	               tvo.abbreviation=switchTeam(String.valueOf(value));
+	               TeamPanel pip=new TeamPanel(tvo,Frame,panelToRemove);
+	               Frame.remove(panelToRemove);
+	               Frame.add(pip);
+	               Frame.repaint();
+	               }
+				 }
 		});
 
 	}
